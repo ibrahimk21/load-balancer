6bef7309acb6588a95941af3fd3f6f44f1202798
Student <student@example.com>
2026-01-10 12:45:00 +0200

    Add CLI argument parsing with random_device seeding

--- main.cpp
+++ main.cpp
@@ -1,10 +1,50 @@
-// Load Balancer Simulation - Main Entry Point
-// M/M/1/N Queue System
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include "Simulation.hpp"
+
+void printUsage(const char* prog) {
+    std::cerr << "Usage: " << prog << " <Time> <M> <P1..PM> <Lambda> <Q1..QM> <Mu1..MuM>" << std::endl;
+}
 
 int main(int argc, char* argv[]) {
-    // TODO: Parse command line arguments
-    // TODO: Initialize simulation
-    // TODO: Run simulation
-    // TODO: Print statistics
+    // Minimum args: prog + Time + M + at least 1 P + Lambda + at least 1 Q + at least 1 Mu
+    // For M servers: 1 + 1 + 1 + M + 1 + M + M = 4 + 3M
+    if (argc < 7) {
+        printUsage(argv[0]);
+        return 1;
+    }
+
+    int idx = 1;
+    double simTime = std::atof(argv[idx++]);
+    int M = std::atoi(argv[idx++]);
+
+    // Expected total args: 1 + 1 + 1 + M + 1 + M + M = 4 + 3M
+    if (argc != 4 + 3 * M) {
+        printUsage(argv[0]);
+        return 1;
+    }
+
+    std::vector<double> P(M);
+    for (int i = 0; i < M; i++) {
+        P[i] = std::atof(argv[idx++]);
+    }
+
+    double lambda = std::atof(argv[idx++]);
+
+    std::vector<int> Q(M);
+    for (int i = 0; i < M; i++) {
+        Q[i] = std::atoi(argv[idx++]);
+    }
+
+    std::vector<double> Mu(M);
+    for (int i = 0; i < M; i++) {
+        Mu[i] = std::atof(argv[idx++]);
+    }
+
+    Simulation sim(simTime, M, P, lambda, Q, Mu);
+    sim.run();
+    sim.printStats();
+
     return 0;
 }
